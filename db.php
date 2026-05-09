320994c0266645d3507fa407af6881063744a13b
Lyzel <talisiclyzel@gmail.com>
2026-05-09 12:21:54 +0800

    Improve error message for database connection failure

--- db.php
+++ db.php
@@ -1,12 +1,17 @@
 <?php
+
 $host = "localhost";
-$db = "exampledb";
+$db   = "exampledb";
 $user = "root";
 $pass = "";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die(json_encode(["error" => "DB connection failed"]));
+
+    die(json_encode([
+        "error" => "Database connection failed"
+    ]));
 }
+
 ?>
